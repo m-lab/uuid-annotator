577ffb138c9dc3e7a1b444c15b8e27f5b9cf73fc
Peter Boothe <pboothe@google.com>
2020-01-09 11:52:59 -0500

    Unify and simplify with FromURL

--- zipfile/provider.go
+++ zipfile/provider.go
@@ -5,6 +5,12 @@ import (
 	"bytes"
 	"errors"
 	"io/ioutil"
+	"net/url"
+)
+
+// Errors that might be returned outside the package.
+var (
+	ErrUnsupportedURLScheme = errors.New("Unsupported URL scheme")
 )
 
 // Provider is the interface implemented by everything that can return a
@@ -16,6 +22,7 @@ type Provider interface {
 	Get() (*zip.Reader, error)
 }
 
+// gcsProvider gets zip files from Google Cloud Storage.
 type gcsProvider struct {
 	bucket, filename string
 }
@@ -24,14 +31,7 @@ func (g *gcsProvider) Get() (*zip.Reader, error) {
 	return nil, errors.New("unimplemented")
 }
 
-// FromGCS returns a zipfile.Provider based on a file in Google Cloud Storage.
-func FromGCS(bucket, filename string) Provider {
-	return &gcsProvider{
-		bucket:   bucket,
-		filename: filename,
-	}
-}
-
+// fileProvider gets zipfiles from the local disk.
 type fileProvider struct {
 	filename string
 }
@@ -44,9 +44,28 @@ func (f *fileProvider) Get() (*zip.Reader, error) {
 	return zip.NewReader(bytes.NewReader(b), int64(len(b)))
 }
 
-// FromFile returns a zipfile.Provider based on a local filename.
-func FromFile(filename string) Provider {
-	return &fileProvider{
-		filename: filename,
+// FromURL returns a new zipfile.Provider based on the passed-in URL. Supported
+// URL schemes are currently: gs://bucket/filename and file:localpath . Whether
+// the path contained in the URL is valid isn't known until the Get() method of
+// the returned Provider is called. Unsupported URL schemes cause this to return
+// ErrUnsupportedURLScheme.
+//
+// Users interested in having the daemon download the data directly from MaxMind
+// should implement an https case in the below handler. M-Lab doesn't need that
+// case because we cache MaxMind's data to reduce load on their servers and to
+// eliminate a runtime dependency on a third party service.
+func FromURL(u *url.URL) (Provider, error) {
+	switch u.Scheme {
+	case "gs":
+		return &gcsProvider{
+			bucket:   u.Host,
+			filename: u.Path,
+		}, nil
+	case "file":
+		return &fileProvider{
+			filename: u.Opaque,
+		}, nil
+	default:
+		return nil, ErrUnsupportedURLScheme
 	}
 }
