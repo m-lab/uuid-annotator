50d5b77eebf864d265e555e8ce459ab8176a5775
Stephen Soltesz <soltesz@google.com>
2020-02-25 17:19:43 -0500

    Add RouteView parser and enable ASN annotations (#18)
    * Add package for parsing and searching routeview data

---
+++ routeview/parse.go
@@ -0,0 +1,127 @@
+package routeview
+
+import (
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"io"
+	"log"
+	"net"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/m-lab/uuid-annotator/annotator"
+)
+
+// IPNet represents a parsed row in a RouteView file.
+type IPNet struct {
+	net.IPNet
+	Systems string
+}
+
+// IPNetSlice is a sortable (and searchable) array of IPNets.
+type IPNetSlice []IPNet
+
+// Len, Less, and Swap make IPNetSlice sortable.
+func (ns IPNetSlice) Len() int {
+	return len(ns)
+}
+func (ns IPNetSlice) Less(i, j int) bool {
+	return bytes.Compare(ns[i].IP, ns[j].IP) < 0
+}
+func (ns IPNetSlice) Swap(i, j int) {
+	n := ns[j]
+	ns[j] = ns[i]
+	ns[i] = n
+}
+
+// ParseSystems converts the RouteView AS string to an annotator.System array.
+// Invalid values are ignored.
+//
+// RouteViews may contain:
+// * a single AS number, e.g. "32", one System with one ASN
+// * an AS set, e.g. "32,54", one System with multiple ASNs
+// * a Multi-Origin AS (MOAS), e.g. "10_20", two Systems each with one or more ASNs.
+func ParseSystems(s string) []annotator.System {
+	// Split systems on "_"
+	systems := strings.Split(s, "_")
+	result := make([]annotator.System, 0, len(systems))
+	for _, asn := range systems {
+		// Split the ASN sets on commas.
+		asns := strings.Split(asn, ",")
+		ints := make([]uint32, 0, len(asns))
+		for _, asn := range asns {
+			value, err := strconv.ParseUint(asn, 10, 32)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			ints = append(ints, uint32(value))
+		}
+		result = append(result, annotator.System{ASNs: ints})
+	}
+	return result
+}
+
+// ParseRouteView reads the given csv file and generates a sorted IP list.
+func ParseRouteView(file []byte) IPNetSlice {
+	result := IPNetSlice{}
+	sm := map[string]string{}
+
+	skip := 0
+	b := bytes.NewBuffer(file)
+	r := csv.NewReader(b)
+	r.Comma = '\t'
+	r.ReuseRecord = true
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if len(record) < 3 {
+			continue
+		}
+		_, n, err := net.ParseCIDR(record[0] + "/" + record[1])
+		if _, ok := sm[record[2]]; !ok {
+			// Break string connection to underlying RAM allocated by the CSV reader.
+			sm[record[2]] = strings.Repeat(record[2], 1)
+		}
+		if len(result) > 1 && result[len(result)-1].Contains(n.IP) && result[len(result)-1].Systems == record[2] {
+			// If the last network contains the current one with the same systems, skip it.
+			skip++
+			continue
+		}
+		result = append(result, IPNet{IPNet: *n, Systems: sm[record[2]]})
+	}
+	log.Println("Skipped:", skip, "routeview netblocks of", len(result)+skip)
+
+	// Sort list so that it can be searched.
+	sort.Sort(result)
+	return result
+}
+
+// ErrNoASNFound is returned when search fails to identify a network for the given src IP.
+var ErrNoASNFound = errors.New("No ASN found for address")
+
+// Search attempts to find the given IP in the IPNetSlice.
+func (ns IPNetSlice) Search(s string) (IPNet, error) {
+	// bytes.Compare will only work correctly when both net.IPs have the same byte count.
+	ip := net.ParseIP(s)
+	if ip.To4() != nil {
+		ip = ip.To4()
+	}
+	node := sort.Search(len(ns), func(i int) bool {
+		if ns[i].Contains(ip) {
+			// Becaue sort.Search finds the lowest index where f(i) is true, we must return
+			// true when the IPNet contains the given IP to prevent off by 1 errors.
+			return true
+		}
+		return bytes.Compare(ns[i].IP, ip) >= 0
+	})
+	if node < len(ns) && ns[node].Contains(ip) {
+		return ns[node], nil
+	}
+	return IPNet{}, ErrNoASNFound
+}
---
+++ routeview/parse_test.go
@@ -0,0 +1,227 @@
+package routeview
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"reflect"
+	"testing"
+
+	"github.com/m-lab/annotation-service/api"
+	"github.com/m-lab/annotation-service/asn"
+	"github.com/m-lab/go/rtx"
+	"github.com/m-lab/uuid-annotator/annotator"
+	"github.com/m-lab/uuid-annotator/rawfile"
+)
+
+var ns IPNetSlice
+var an api.Annotator
+
+func init() {
+	var err error
+	// Load file in setup for Benchmark.
+	b, err := ioutil.ReadFile("../testdata/RouteViewIPv4.pfx2as.gz")
+	rtx.Must(err, "Failed to read routeview data")
+	b2, err := rawfile.FromGZ(b)
+	rtx.Must(err, "Failed to decompress routeview")
+	ns = ParseRouteView(b2)
+
+	// Only used for Benchmark.
+	an, err = asn.LoadASNDatasetFromReader(bytes.NewBuffer(b2))
+	rtx.Must(err, "Failed to load api.Annotator")
+
+	log.SetFlags(0)
+}
+
+func TestParseRouteView(t *testing.T) {
+	tests := []struct {
+		name      string
+		filename  string
+		wantCount int
+	}{
+		{
+			name:      "success-ipv4",
+			filename:  "../testdata/RouteViewIPv4.pfx2as.gz",
+			wantCount: 545957,
+		},
+		{
+			name:      "success-ipv6",
+			filename:  "../testdata/RouteViewIPv6.pfx2as.gz",
+			wantCount: 54317,
+		},
+		{
+			name:      "corrupt-ipv4",
+			filename:  "../testdata/RouteViewIPv4.corrupt.gz",
+			wantCount: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gz, err := ioutil.ReadFile(tt.filename)
+			rtx.Must(err, "Failed to read routeview data")
+			b, err := rawfile.FromGZ(gz)
+			rtx.Must(err, "Failed to decompress routeview")
+
+			ns := ParseRouteView(b)
+			if len(ns) != tt.wantCount {
+				t.Errorf("Parse() = %v, want %v", len(ns), tt.wantCount)
+			}
+		})
+	}
+}
+
+func TestParseSystems(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want []annotator.System
+	}{
+		{
+			name: "success",
+			s:    "12345",
+			want: []annotator.System{
+				{ASNs: []uint32{12345}},
+			},
+		},
+		{
+			name: "success-multiple-as",
+			s:    "12345,54321",
+			want: []annotator.System{
+				{ASNs: []uint32{12345, 54321}},
+			},
+		},
+		{
+			name: "success-multi-origin-as",
+			s:    "12345_54321",
+			want: []annotator.System{
+				{ASNs: []uint32{12345}},
+				{ASNs: []uint32{54321}},
+			},
+		},
+		{
+			name: "success-ignore-bad-format",
+			s:    "this-is-not-an-asn",
+			want: []annotator.System{
+				{ASNs: []uint32{}},
+			},
+		},
+		{
+			name: "success-skip-bad-format",
+			s:    "1234,this-is-not-an-asn",
+			want: []annotator.System{
+				{ASNs: []uint32{1234}},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParseSystems(tt.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseSystems() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIndex_Search(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		filename string
+		src      string
+		want     IPNet
+		wantErr  bool
+	}{
+		{
+			name:     "success",
+			filename: "../testdata/RouteViewIPv4.pfx2as.gz",
+			src:      "1.0.192.1",
+			want: IPNet{
+				IPNet:   net.IPNet{IP: net.ParseIP("1.0.128.0").To4(), Mask: net.CIDRMask(17, 32)},
+				Systems: "23969",
+			},
+		},
+		{
+			name:     "success-ipv6",
+			filename: "../testdata/RouteViewIPv6.pfx2as.gz",
+			src:      "2001:200::1",
+			want: IPNet{
+
+				IPNet:   net.IPNet{IP: net.ParseIP("2001:200::"), Mask: net.CIDRMask(32, 128)},
+				Systems: "2500",
+			},
+		},
+		{
+			name:     "error-not-found-ipv6",
+			filename: "../testdata/RouteViewIPv4.pfx2as.gz",
+			src:      "2001:ff00::1", // IPv6 address will not be found in IPv4 views.
+			wantErr:  true,
+		},
+		{
+			name:     "error-not-found-ipv4",
+			filename: "../testdata/RouteViewIPv4.pfx2as.gz",
+			src:      "9.0.0.9", // not present in IPv4 route view.
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gz, err := ioutil.ReadFile(tt.filename)
+			rtx.Must(err, "Failed to read routeview data")
+			b, err := rawfile.FromGZ(gz)
+			rtx.Must(err, "Failed to decompress routeview")
+			ns := ParseRouteView(b)
+
+			got, err := ns.Search(tt.src)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Index.Search() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				return
+			}
+			if !reflect.DeepEqual(got.IPNet, tt.want.IPNet) {
+				t.Errorf("Index.Search() = %v, want %v", got.IPNet, tt.want.IPNet)
+			}
+			if got.Systems != tt.want.Systems {
+				t.Errorf("Index.Search() returned wrong Systems = %q, want %q", got.Systems, tt.want.Systems)
+			}
+		})
+	}
+}
+
+func BenchmarkSearch(b *testing.B) {
+	found := 0
+	missing := 0
+	src := "1.0.192.1"
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, err := ns.Search(src)
+		if err != nil {
+			missing++
+		} else {
+			found++
+		}
+		_ = ParseSystems(r.Systems)
+	}
+	fmt.Println("f:", found, "m:", missing)
+}
+
+func BenchmarkAnnotate(b *testing.B) {
+	found := 0
+	missing := 0
+	src := "1.0.192.1"
+	ann := api.Annotations{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := an.Annotate(src, &ann)
+		if err != nil {
+			missing++
+		} else {
+			found++
+		}
+		ann.Network = nil
+	}
+	fmt.Println("f:", found, "m:", missing)
+}
